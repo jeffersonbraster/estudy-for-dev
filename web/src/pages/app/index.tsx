fda77ca80315447481502f2458edeb75a469defa
Jefferson <jeffersonbraster@gmail.com>
2022-04-04 21:37:47 -0300

    Criando boilerplate do backend com apollo, graphql, auth0 e iniciando front com auth0

---
+++ web/src/pages/app/index.tsx
@@ -0,0 +1,31 @@
+import { useUser, withPageAuthRequired } from "@auth0/nextjs-auth0";
+import { GetServerSideProps } from "next";
+import React from "react";
+
+export default function Home() {
+  const { user } = useUser();
+
+  return (
+    <>
+      <div>Bem vindo,</div> <pre>{JSON.stringify(user)}</pre>
+    </>
+  );
+}
+
+export const getServerSideProps: GetServerSideProps = withPageAuthRequired();
+// export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
+//   const session = getSession(req, res);
+
+//   if (!session) {
+//     return {
+//       redirect: {
+//         destination: "/api/aith/login",
+//         permanent: false,
+//       },
+//     };
+//   }
+
+//   return {
+//     props: {},
+//   };
+// };
